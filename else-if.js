6c289be730a03ed0aeaa294620b3289d4c52f4b7
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-06-28 01:44:44 +0530

    Added else if statements

---
+++ else-if.js
@@ -0,0 +1,11 @@
+var myAge = 21;
+
+if (myAge >= 18) {
+    document.write("You are old enough to vote!");
+} else if (myAge >= 16) {
+    document.write("You are old enough to drive!");
+} else if (myAge >= 13) {
+    document.write("You are teenager!");
+} else {
+    document.write("You are not teenager");   
+}
