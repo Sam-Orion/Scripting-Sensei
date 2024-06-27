8eed1d25c8a0d7b8a901efce60c157fee641b1f9
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-06-27 17:50:40 +0530

    Hello

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
+// the first alert
 alert("Hello World!");
+
+// the second alert
 alert("Hi Again");
+
+// make the image invisible
+// move the image from top to bottom
+// make the image visible
+
+var myVariable;
+myVariable = 10;
+var myVar2 = "Hello";
+myVar2;
