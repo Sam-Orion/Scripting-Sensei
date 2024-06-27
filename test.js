e33ce3d106c058f10e7ee4c438a58d05e2ff7f51
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-06-28 01:35:33 +0530

    Learnt if-else statement

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
-// the first alert
-alert("Hello World!");
+// // the first alert
+// alert("Hello World!");
 
-// the second alert
-alert("Hi Again");
+// // the second alert
+// alert("Hi Again");
 
-// make the image invisible
-// move the image from top to bottom
-// make the image visible
+// // make the image invisible
+// // move the image from top to bottom
+// // make the image visible
+
+var youLikeMe = true;
+var myNum = 11
+
+if (youLikeMe){
+
+    document.write("You like me!");
+
+}
+
+if (myNum = 10){
+    document.write("myNum is equal to 10");
+} else{
+    document.write("myNum is not equal to 10");
+}
