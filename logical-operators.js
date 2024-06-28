49916d3700925200b882dd2f1429ba84a99dc96b
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-06-28 21:47:55 +0530

    Added some basic logical operators

--- logical-operators.js
+++ logical-operators.js
@@ -1,7 +1,15 @@
-var myAge = 25;
+var myAge = 35;
 
 if (myAge >= 18 && myAge <= 30){
     document.write("You can come, you cool dude");
 } else {
     document.write("you ain't cool enough to come");
+}
+
+var myAge = 15;
+
+if (myAge < 18 || myAge > 30 || myAge === 25){
+    document.write("You ain't coming!");
+} else {
+    document.write("You can come, you cool dude");
 }
