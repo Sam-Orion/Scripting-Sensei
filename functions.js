9be2d4627d50930a9226735a5c425f29eb3c8a76
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-06-29 22:00:52 +0530

    Variable Scope

--- functions.js
+++ functions.js
@@ -1,10 +1,20 @@
+var average = 0; // global variable
+
 function getAverage (a,b){
 
-    var average = (a + b)/2;
+    average = (a + b)/2; 
     console.log(average);
     return average;
 
 }
 
-var myResult = getAverage(7,8);
-console.log("The average is " + myResult);
+var myResult = getAverage(7,8); // global variable
+
+
+// console.log(average); // undefined
+
+function logResult(){
+    console.log("The average is " + myResult);
+}
+
+logResult();
