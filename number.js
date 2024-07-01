15fbc2af75ede5f6af9dbf2c4a2974564cc4e280
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-07-01 21:38:48 +0530

    number.js

---
+++ number.js
@@ -0,0 +1,14 @@
+var a = 5;
+var b = 5;
+
+console.log(a+b);
+
+console.log(Math.round(7.5));
+
+console.log(Math.floor(7.5));
+
+console.log(Math.ceil(7.5));
+
+console.log(Math.max(7, 5, 6, 9, 0));
+
+console.log(Math.PI);
